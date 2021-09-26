5485b6a5d32c90b111950d999436099d62110253
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2021-09-26 10:38:12 +0200

    Add components to appcomponent, add declare and imports on appmodule and add routes for home, search and detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
+import { SearchComponent } from '../app/pages/search/search.component';
+import { DetailComponent} from '../app/pages/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'search', component:SearchComponent},
+  {path:'detail/:id', component:DetailComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchComponent } from './pages/search/search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FlagComponent } from './components/flag/flag.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SearchComponent
+    SearchComponent,
+    FlagComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
