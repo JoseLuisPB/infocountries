bf2d02d3978f87e7fd8c813ce7c33abc3e2de058
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2021-10-06 19:56:30 +0200

    Add format and functionality to detail component

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ICountry } from 'src/app/interfaces/country';
 import { RestcountriesService } from 'src/app/services/restcountries.service';
 
 @Component({
@@ -9,15 +10,17 @@ import { RestcountriesService } from 'src/app/services/restcountries.service';
 })
 export class DetailComponent implements OnInit {
 
-  country: any = {
+  languagesFormated = '';
+
+  country: ICountry = {
     flag: '',
     name: '',
     capital:'',
+    languages: [],
     population: 0,
     area: 0,
     region: '',
     subregion: '',
-    continent: '',
   };
 
   constructor(private restcountries: RestcountriesService,
@@ -25,6 +28,7 @@ export class DetailComponent implements OnInit {
   {
     const id = this.route.snapshot.params.id;
     this.restcountries.getCountry(id).subscribe( (data:any) => {
+
       this.country.flag = data[0].flags[1];
       this.country.name = data[0].name.common;
       this.country.capital = data[0].capital[0];
@@ -32,7 +36,14 @@ export class DetailComponent implements OnInit {
       this.country.area = data[0].area;
       this.country.region = data[0].region;
       this.country.subregion = data[0].subregion;
-      this.country.continent = data[0].continent;
+
+      // Convert the object of languages to an array to iterate
+      const entries = Object.entries(data[0].languages)
+      entries.forEach( item => {
+        this.country.languages.push(String(item[1]));
+      });
+
+      this.languagesFormated = this.country.languages.join(', ');
     })
   }
 
