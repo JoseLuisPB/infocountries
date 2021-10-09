0232bf358ee30cd73bdc5f859bda057fd4b60fad
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2021-10-09 11:11:46 +0200

    Add subscriptions array to components

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ICountry } from 'src/app/interfaces/country';
 import { RestcountriesService } from 'src/app/services/restcountries.service';
 
@@ -8,8 +9,9 @@ import { RestcountriesService } from 'src/app/services/restcountries.service';
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
 
+  subscriptions: Subscription[] = [];
   languagesFormated = '';
 
   country: ICountry = {
@@ -24,30 +26,38 @@ export class DetailComponent implements OnInit {
   };
 
   constructor(private restcountries: RestcountriesService,
-              private route: ActivatedRoute)
+              private route: ActivatedRoute
+  )
   {
     const id = this.route.snapshot.params.id;
-    this.restcountries.getCountry(id).subscribe( (data:any) => {
+    this.subscriptions.push(
+      this.restcountries.getCountry(id).subscribe( (data:any) => {
 
-      this.country.flag = data[0].flags[1];
-      this.country.name = data[0].name.common;
-      this.country.capital = data[0].capital[0];
-      this.country.population = data[0].population;
-      this.country.area = data[0].area;
-      this.country.region = data[0].region;
-      this.country.subregion = data[0].subregion;
+        this.country.flag = data[0].flags[1];
+        this.country.name = data[0].name.common;
+        this.country.capital = data[0].capital[0];
+        this.country.population = data[0].population;
+        this.country.area = data[0].area;
+        this.country.region = data[0].region;
+        this.country.subregion = data[0].subregion;
 
-      // Convert the object of languages to an array to iterate
-      const entries = Object.entries(data[0].languages)
-      entries.forEach( item => {
-        this.country.languages.push(String(item[1]));
-      });
+        // Convert the object of languages to an array to iterate
+        const entries = Object.entries(data[0].languages)
+        entries.forEach( item => {
+          this.country.languages.push(String(item[1]));
+        });
 
-      this.languagesFormated = this.country.languages.join(', ');
-    })
+        this.languagesFormated = this.country.languages.join(', ');
+      })
+    );
+
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void{
+    this.subscriptions.forEach( subscription => subscription.unsubscribe());
   }
 
 }
