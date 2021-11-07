892b85dffc0e6a2c91aa882073c4340848c92840
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2021-11-07 11:22:51 +0100

    Adjust country interface

--- src/app/interfaces/country.ts
+++ src/app/interfaces/country.ts
@@ -1,7 +1,7 @@
 export interface ICountry {
-  code: string;
-  flag: string;
-  name: string;
+  code?: string;
+  flag?: string;
+  name?: string;
   capital?: string;
   languages?: string[];
   population?: number;
